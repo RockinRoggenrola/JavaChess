9b84048cb154ed223055b40561018efae363d426
Tanvir Ahmed <tahme8766@gmail.com>
2022-06-19 17:46:09 -0400

    Added methods to the Board class

--- Board.java
+++ Board.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class Board {
     String[] squares = new String[64];
     //a1 - index 0
@@ -6,43 +8,66 @@ class Board {
     //b1 - index 8
     //...
     //h8 - index 63
+    HashMap<Integer, String> whitePieces = new HashMap<>();
+    HashMap<Integer, String> blackPieces = new HashMap<>();
+
+    public void addWhite(int square, String piece) {
+        this.squares[square] = "w" + piece;
+        this.whitePieces.put(square, piece);
+    }
+
+    public void addBlack(int square, String piece) {
+        this.squares[square] = "b" + piece;
+        this.blackPieces.put(square, piece);
+    }
+
+    public void removePiece(int square) {
+        char color = Square.color(this, square);
+        if (color == 'w') {
+            this.squares[square] = null;
+            this.whitePieces.remove(square);
+        }
+        else if (color == 'b') {
+            this.squares[square] = null;
+            this.blackPieces.remove(square);
+        }
+    }
 
     public void setup() {
-        this.squares[0] = "wr";
-        this.squares[7] = "wr";
-        this.squares[1] = "wn";
-        this.squares[6] = "wn";
-        this.squares[2] = "wb";
-        this.squares[5] = "wb";
-        this.squares[3] = "wq";
-        this.squares[4] = "wk";
+        this.addWhite(0, "r");
+        this.addWhite(7, "r");
+        this.addWhite(1, "n");
+        this.addWhite(6, "n");
+        this.addWhite(2, "b");
+        this.addWhite(3, "q");
+        this.addWhite(4, "k");
 
-        this.squares[8] = "wp";
-        this.squares[9] = "wp";
-        this.squares[10] = "wp";
-        this.squares[11] = "wp";
-        this.squares[12] = "wp";
-        this.squares[13] = "wp";
-        this.squares[14] = "wp";
-        this.squares[15] = "wp";
+        this.addWhite(8, "p");
+        this.addWhite(9, "p");
+        this.addWhite(10, "p");
+        this.addWhite(11, "p");
+        this.addWhite(12, "p");
+        this.addWhite(13, "p");
+        this.addWhite(14, "p");
+        this.addWhite(15, "p");
 
-        this.squares[63] = "br";
-        this.squares[56] = "br";
-        this.squares[62] = "bn";
-        this.squares[57] = "bn";
-        this.squares[61] = "bb";
-        this.squares[58] = "bb";
-        this.squares[59] = "bq";
-        this.squares[60] = "bk";
+        this.addBlack(63, "r");
+        this.addBlack(56, "r");
+        this.addBlack(62, "n");
+        this.addBlack(57, "n");
+        this.addBlack(61, "b");
+        this.addBlack(58, "b");
+        this.addBlack(59, "q");
+        this.addBlack(60, "k");
 
-        this.squares[55] = "bp";
-        this.squares[54] = "bp";
-        this.squares[53] = "bp";
-        this.squares[52] = "bp";
-        this.squares[51] = "bp";
-        this.squares[50] = "bp";
-        this.squares[49] = "bp";
-        this.squares[48] = "bp";
+        this.addBlack(55, "p");
+        this.addBlack(54, "p");
+        this.addBlack(53, "p");
+        this.addBlack(52, "p");
+        this.addBlack(51, "p");
+        this.addBlack(50, "p");
+        this.addBlack(49, "p");
+        this.addBlack(48, "p");
     }
 
 }
